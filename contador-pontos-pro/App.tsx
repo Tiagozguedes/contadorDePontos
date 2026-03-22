bae3dbbccbd0f77e84e9e99536bde4166b7fe1f3
TiagozGuedes <tiagozguedes@gmail.com>
2026-03-22 00:00:09 -0300

    Corrigida a versão do tailwindcss e do nativewind para rodar o projeto

--- contador-pontos-pro/App.tsx
+++ contador-pontos-pro/App.tsx
@@ -3,7 +3,10 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
+    <View
+      className="flex-1 items-center justify-center bg-gray-900"
+      style={{ flex: 1, backgroundColor: '#111827' }}
+    >
       <Text className="text-white text-2xl font-bold">
         Contador de Pontos Pro 🎮
       </Text>
